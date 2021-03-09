80d06bc554ddf352f31144961580c9c4341d108a
Vin Lee <vin1974@gmail.com>
2021-03-08 20:38:12 -0500

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/App.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "vin/prj/routingTest/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("vin.prj.routingTest.controller.App", {});
+});
